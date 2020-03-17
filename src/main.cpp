894fab7c9501ca0e49b33a1ab3024846edfacbcd
lzhmarkjen <42379096+lzhmarkjen@users.noreply.github.com>
2020-03-17 09:51:42 +0800

    完成了IO

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,43 @@
-#include <fstream>
 #include <vector>
 #include "container.h"
+#include "IOHandler.h"
 
 #define DEBUG 0
 
 int main(int argc, char* argv[]) {
-	ifstream input;
-	ofstream output;
+	IOHandler io(argc, argv, DEBUG);
 
-	if (DEBUG) {
-		input.open("input.txt");
-		output.open("output.txt");
-	} else {
-		for (int arg = 0; arg < argc; arg++) {
-			if ((string)argv[arg] == "-i") {
-				input.open(argv[arg + 1]);
-			} else if ((string)argv[arg] == "-o") {
-				output.open(argv[arg + 1]);
-			}
-		}
-	}
-
-	int line;
-	input >> line;
+	int line = io.readInt();
 
 	Container* container = new Container;
 	vector<Graph*> graphs;
 
 	for (int i = 0; i < line; i++) {
-		char type;
 		Graph* graph1 = NULL;
-		input >> type;
+		char type = io.readChar();
 		if (type == 'L') {
-			int x1, y1, x2, y2;
-			input >> x1 >> y1 >> x2 >> y2;
+			int x1 = io.readInt();
+			int y1 = io.readInt();
+			int x2 = io.readInt();
+			int y2 = io.readInt();
 			Dot d1(x1, y1);
 			Dot d2(x2, y2);
 			graph1 = new Line(d1, d2);
 
 		} else if (type == 'R') {
-			int x1, y1, x2, y2;
-			input >> x1 >> y1 >> x2 >> y2;
+			int x1 = io.readInt();
+			int y1 = io.readInt();
+			int x2 = io.readInt();
+			int y2 = io.readInt();
 			Dot d1(x1, y1);
 			Dot d2(x2, y2);
 			graph1 = new Radial(d1, d2);
 
 		} else if (type == 'S') {
-			int x1, y1, x2, y2;
-			input >> x1 >> y1 >> x2 >> y2;
+			int x1 = io.readInt();
+			int y1 = io.readInt();
+			int x2 = io.readInt();
+			int y2 = io.readInt();
 			Dot d1(x1, y1);
 			Dot d2(x2, y2);
 			graph1 = new Segment(d1, d2);
@@ -64,7 +54,7 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
-	output << container->Size() << endl;
+	io.outputInt(container->Size());
 	// cout << "total: " << container->Size() << endl;
 	return 0;
 }
