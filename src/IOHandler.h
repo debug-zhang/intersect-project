f6592c2d82828a6a5c91c38b0c37bb8ecf630d75
lzhmarkjen <42379096+lzhmarkjen@users.noreply.github.com>
2020-03-17 09:28:22 +0800

    添加了IO

---
+++ src/IOHandler.h
@@ -0,0 +1,15 @@
+#include <fstream>
+
+using namespace std;
+
+class IOHandler {
+private:
+	ifstream ifstream;
+	ofstream ofstream;
+
+public:
+	IOHandler(int argc, char* argv[], bool DEBUG);
+	int readInt();
+	char readChar();
+	void outputInt(const int n);
+};
