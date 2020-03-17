7803b15bd218c00b7d2bdb20f4b24ba6812274bf
lzhmarkjen <42379096+lzhmarkjen@users.noreply.github.com>
2020-03-17 09:37:42 +0800

    Revert "添加了IO"
    This reverts commit f6592c2d82828a6a5c91c38b0c37bb8ecf630d75.

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,53 @@
+#include <fstream>
 #include <vector>
 #include "container.h"
-#include "IOHandler.h"
 
 #define DEBUG 0
 
 int main(int argc, char* argv[]) {
-	IOHandler io(argc, argv, DEBUG);
+	ifstream input;
+	ofstream output;
 
-	int line = io.readInt();
+	if (DEBUG) {
+		input.open("input.txt");
+		output.open("output.txt");
+	} else {
+		for (int arg = 0; arg < argc; arg++) {
+			if ((string)argv[arg] == "-i") {
+				input.open(argv[arg + 1]);
+			} else if ((string)argv[arg] == "-o") {
+				output.open(argv[arg + 1]);
+			}
+		}
+	}
+
+	int line;
+	input >> line;
 
 	Container* container = new Container;
 	vector<Graph*> graphs;
 
 	for (int i = 0; i < line; i++) {
+		char type;
 		Graph* graph1 = NULL;
-		char type = io.readChar();
+		input >> type;
 		if (type == 'L') {
-			int x1 = io.readInt();
-			int y1 = io.readInt();
-			int x2 = io.readInt();
-			int y2 = io.readInt();
+			int x1, y1, x2, y2;
+			input >> x1 >> y1 >> x2 >> y2;
 			Dot d1(x1, y1);
 			Dot d2(x2, y2);
 			graph1 = new Line(d1, d2);
 
 		} else if (type == 'R') {
-			int x1 = io.readInt();
-			int y1 = io.readInt();
-			int x2 = io.readInt();
-			int y2 = io.readInt();
+			int x1, y1, x2, y2;
+			input >> x1 >> y1 >> x2 >> y2;
 			Dot d1(x1, y1);
 			Dot d2(x2, y2);
 			graph1 = new Radial(d1, d2);
 
 		} else if (type == 'S') {
-			int x1 = io.readInt();
-			int y1 = io.readInt();
-			int x2 = io.readInt();
-			int y2 = io.readInt();
+			int x1, y1, x2, y2;
+			input >> x1 >> y1 >> x2 >> y2;
 			Dot d1(x1, y1);
 			Dot d2(x2, y2);
 			graph1 = new Segment(d1, d2);
@@ -54,7 +64,7 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
-	io.outputInt(container->Size());
+	output << container->Size() << endl;
 	// cout << "total: " << container->Size() << endl;
 	return 0;
 }
