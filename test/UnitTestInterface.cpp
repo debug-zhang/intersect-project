d9d90b0b4c1f573edf1a0d7576f3fa7aaa3fe5be
blackbird52 <xinzezhang@outlook.com>
2020-03-18 01:04:58 +0800

    完成DLL的封装与单元测试

---
+++ test/UnitTestInterface.cpp
@@ -0,0 +1,59 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../IntersectProject/core/container.h"
+
+class _declspec(dllexport) Container;
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		TEST_METHOD(TestAddGraphAndSize) {
+			Container* container = new Container();
+			container->AddGraph('S', -1, 3, 2, -1);
+			container->AddGraph('L', -2, 2, 3, 0);
+			container->AddGraph('R', -3, 0, 4, 2);
+			Assert::AreEqual(container->Size(), 1);
+
+			container = new Container();
+			container->AddGraph('S', 1, 1, 1, -1);
+			container->AddGraph('L', 2, 2, 2, 0);
+			container->AddGraph('R', 3, 3, 3, 4);
+			Assert::AreEqual(container->Size(), 0);
+
+			container = new Container();
+			container->AddGraph('S', 2, 4, 3, 2);
+			container->AddGraph('L', -1, 4, 5, 2);
+			container->AddGraph('R', 2, 5, -1, 2);
+			Assert::AreEqual(container->Size(), 2);
+
+			container = new Container();
+			container->AddGraph('S', 0, 2, 3, -1);
+			container->AddGraph('L', 0, 2, 2, 2);
+			container->AddGraph('R', 0, 2, 3, -1);
+			Assert::AreEqual(container->Size(), 1);
+
+			container = new Container();
+			container->AddGraph('S', 0, 2, 3, -1);
+			container->AddGraph('L', 3, -1, 4, 2);
+			container->AddGraph('R', 0, 2, 3, -1);
+			Assert::AreEqual(container->Size(), 1);
+
+			container = new Container();
+			container->AddGraph('S', 0, 2, 3, -1);
+			container->AddGraph('L', -1, 3, -5, 0);
+			container->AddGraph('R', 0, 2, 3, -1);
+			Assert::AreEqual(container->Size(), 0);
+
+			container = new Container();
+			container->AddGraph('S', 0, 2, 3, -1);
+			container->AddGraph('L', 4, 2, 5, 0);
+			container->AddGraph('R', 0, 2, 3, -1);
+			Assert::AreEqual(container->Size(), 1);
+		}
+	};
+
+}
